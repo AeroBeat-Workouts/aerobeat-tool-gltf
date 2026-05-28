8794f3dcd8bac31acf45d31f1de3deff4d79f1b6
Derrick Barra <gambitgamesllc@gmail.com>
2026-05-28 09:34:30 -0400

    Replace testbed src symlink with self-mounted addon

--- .testbed/scripts/multi_gltf_proving_surface.gd
+++ .testbed/scripts/multi_gltf_proving_surface.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 const TOOL_CANDIDATE_PATHS := [
-	"res://src/AeroGLTFTool.gd",
+	"res://addons/aerobeat-tool-gltf-loader/src/AeroGLTFTool.gd",
 	"res://addons/aerobeat-tool-gltf-loader/src/AeroGLTFTool.gd",
 ]
 const FIXTURE_PATH := "res://fixtures/models/alien-planet.glb"
